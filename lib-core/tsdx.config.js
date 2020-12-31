a5b206cf37d50d2027581a13585a349a6bfcc91f
Clint Harris <clintharris@users.noreply.github.com>
2020-12-31 15:46:35 -0500

    Modified tsdx config so that Rollup will expect uuidv4 and murmurhash as external libraries and stop displaying a warning message when builds are done. Deleted uuid and murmurhash files from repo; modified example app to bring this in from unpkg.com instead.

--- lib-core/tsdx.config.js
+++ lib-core/tsdx.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  // This function will run for each entry/format/env combination
+  // This function will run for each entry/format/env combination. See https://tsdx.io/customization#rollup
   rollup(config, options) {
-    return config; // always return a config.
+    console.log('ℹ️ [tsdx.config.js] Configuring build to NOT include external libraries (uuid, murmurhash, etc.)');
+    config.external = ['uuidv4', 'murmurhash'];
+    return config;
   },
 };
