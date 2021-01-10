ec95c5a30911b696439a22c2e1af90c50d6f48b3
Clint Harris <clintharris@users.noreply.github.com>
2021-01-10 15:54:42 -0500

    Changed oplog store and index names. Modified keyPath used for store and index.

--- lib-core/src/db.ts
+++ lib-core/src/db.ts
@@ -3,11 +3,12 @@ import { HLTime } from './HLTime';
 import { makeNodeId } from './utils';
 
 export enum STORE_NAME {
-  META = 'IDBSideSync_UNSYNCED_MetaStore',
-  OPLOG = 'IDBSideSync_SYNCED_OpLogStore',
+  META = 'IDBSideSync_MetaStore',
+  OPLOG = 'IDBSideSync_OpLogStore',
 }
 
 export const OPLOG_STORE = STORE_NAME.OPLOG;
+export const OPLOG_INDEX = 'Indexed by: store, objectKey, prop, hlcTime';
 
 let dbSingleton: IDBDatabase;
 let settings: { nodeId: string };
@@ -29,8 +30,31 @@ export function onupgradeneeded(event: IDBVersionChangeEvent): void {
   // Use `keyof OpLogEntry` type in case OpLogEntry props are ever renamed. Note that the FIRST key (and only the first
   // key) in the `keyPath` determines how the objects will be sorted by default (e.g., the order they'd appear in
   // `getAll()` results).
-  const keyPath: Array<keyof OpLogEntry> = ['hlcTime', 'store', 'objectKey', 'prop'];
-  db.createObjectStore(STORE_NAME.OPLOG, { keyPath });
+  const storeKeyPath: keyof OpLogEntry = 'hlcTime';
+  const oplogStore = db.createObjectStore(STORE_NAME.OPLOG, { keyPath: storeKeyPath });
+
+  // The purpose of this index will be to find the most recent oplog entry for a given store + object key + prop. This
+  // means that:
+  //
+  //  1. The index will have an entry for each object in the object store that has a "non-empty" value for EVERY prop
+  //     (i.e., the object needs to have the prop defined and the value is not null, undefined, or a boolean).
+  //  2. The index key for the object will be an array consisting of the values for the corresponding prop values.
+  //  3. The index is sorted by the keys. In this case, each key is an array; the IndexedDB spec defines an algo for how
+  //     arrays are compared and sorted (see https://www.w3.org/TR/IndexedDB/#key-construct).
+  //  4. This basically amounts comparing each element of both arrays, using the same comparison algo.
+  //  5. Note that the IndexedDB comparison algo sometimes determines order based on the TYPE of a thing, not its value
+  //     (e.g., an array is considered "greater than" a string). This matters for the 'objectKey' prop since that prop
+  //     value could be a string, number, Date, or array of those things.
+  //
+  // Since our only use case will be searching for entries that have a matching store + objectKey + prop (and needing
+  // those to be sorted by hlcTime so we can get the most recent), we aren't concerned with how the index entries
+  // initially sorted based on 'store' and 'objectKey' (i.e., we don't care if the index key for Object A comes before
+  // the one for Object B because of their 'objectKey' values). We only care that all of oplog entries for a specific
+  // object and prop are grouped together.
+  //
+  // For more info see https://stackoverflow.com/a/15625231/62694.
+  const indexKeyPath: Array<keyof OpLogEntry> = ['store', 'objectKey', 'prop', 'hlcTime'];
+  oplogStore.createIndex(OPLOG_INDEX, indexKeyPath);
 }
 
 export async function init(db: IDBDatabase): Promise<void> {
