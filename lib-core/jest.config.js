e270175190b906040c964bc43c50e58d43e241bb
Clint Harris <clintharris@users.noreply.github.com>
2021-01-15 08:42:46 -0500

    Modified lib-core jest config to generate coverage report.

--- lib-core/jest.config.js
+++ lib-core/jest.config.js
@@ -8,4 +8,8 @@ module.exports = {
   },
   testRegex: '(/test/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'jest-coverage',
 };
